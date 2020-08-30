7338aa7bdc7596bb96975035c680715a0c4df4ea
Sinharitik589 <sinharitik18112835@gmail.com>
2020-08-30 23:35:49 +0530

    Updates

---
+++ _next/static/qxo5O9fH1WlCf98zs_c6K/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-32f910e7b62af6871d0b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ff3673b4e910f9435a1e.js"],"/about":["static\u002Fchunks\u002Fpages\u002Fabout-8512a7c4f585107d0f2c.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
